1fb36dd53c2bebfc517161da02701b53938e34fd
Ruka257 <r.akingbala52@gmail.com>
2022-01-21 14:15:45 +0000

    #comment commit again!

--- fri21script.sh
+++ fri21script.sh
@@ -1,17 +1,12 @@
 #!bin/bash
 
+echo "enter a grade"
 read grade
 
 case $grade in
-
-
-    "A") echo "well done";;
-​
-    "B")  echo  "great work";;
-
-    "C" echo "not bad";;
-
-    *) echo "Not recognised";;
-
-    esac
+"A") echo "well done";;
+"B") echo "great work";;
+"C") echo "not bad";;
+*) echo "Not recognised";;
+esac
 
